5d8188f9e4a1900f580c30a24fe38c06a9e831a5
@anninwonderland <anninwonderland@yandex.ru>
2019-06-27 21:46:16 +0300

    Initial Commit

---
+++ project16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project16
+//
+//  Created by Ann on 27/06/2019.
+//  Copyright © 2019 Ann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
